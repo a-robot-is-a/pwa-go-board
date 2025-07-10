efec112532fd23880094133ad96647851d68c94a
a-robot-is-a <peter.hartmann.1a@gmail.com>
2025-07-10 19:42:37 +0200

    Create generate-config.js

---
+++ generate-config.js
@@ -0,0 +1,6 @@
+// generate-config.js
+const fs = require('fs');
+const serverURL = process.env.SERVER_URL;
+
+const configContent = `window.SERVER_ADDRESS = "${serverURL}";`;
+fs.writeFileSync('docs/config.js', configContent);
