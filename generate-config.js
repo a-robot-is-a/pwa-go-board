6743ec127e6b13e332860f2b7eb3bdbba8612665
a-robot-is-a <peter.hartmann.1a@gmail.com>
2025-07-10 21:35:24 +0200

    Update generate-config.js

--- generate-config.js
+++ generate-config.js
@@ -1,6 +1,10 @@
-// generate-config.js
 const fs = require('fs');
 const serverURL = process.env.SERVER_URL;
+
+if (!serverURL) {
+  console.error("❌ SERVER_URL nicht gesetzt!");
+  process.exit(1);
+}
 
 const configContent = `window.SERVER_ADDRESS = "${serverURL}";`;
 fs.writeFileSync('config.js', configContent);
